9fe25a1ebc03a34a0da85d4deccb730ae4c20002
samuellagee <samuellage07@gmail.com>
2026-03-17 16:31:31 -0300

    TP1 - Alteracao Aleatoria

---
+++ tp1-codigos/AlteracaoAleatoria.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class AlteracaoAleatoria {
+
+	public static String sorteio(String s, Random gerador) {
+		String trocada = "";
+		char l1 = ((char)('a' + (Math.abs(gerador.nextInt()) % 26)));
+		char l2 = ((char)('a' + (Math.abs(gerador.nextInt()) % 26)));
+
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) == l1){
+				trocada = trocada + l2;
+			}else trocada = trocada + s.charAt(i);
+		}
+		return trocada;
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		Random gerador = new Random();
+		gerador.setSeed(4);
+
+		String s = sc.nextLine();
+
+		while (!(s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M')) {
+			System.out.println(sorteio(s, gerador));
+			System.out.println();
+			s = sc.nextLine();
+
+		}
+		sc.close();
+	}
+}
